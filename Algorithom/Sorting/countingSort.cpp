d6218bcf0ac4b5d35cefcba43f0525388087b41b
Mahfuz001 <likhon.codew@gmail.com>
2019-02-27 08:47:30 +0600

    Learned CountingSort

--- Algorithom/Sorting/countingSort.cpp
+++ Algorithom/Sorting/countingSort.cpp
@@ -15,6 +15,8 @@ using namespace std;
 const double pi = 2*acos(0.0);
 const long long mod = 1e9+7;
 
+ifstream in("input.in");
+
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
@@ -24,6 +26,26 @@ int main(){
     freopen("input.txt","r",stdin);
     //freopen("output.txt","w",stdout);
     #endif
+    
+    int n;
+    in>>n;
+    vector<int> nums(n);
+    //memset(nums, 0, nums.size());
+    for(int i=1;i<=n;i++){
+        int x;
+        in>>x;
+        nums[x]++;
+    }
+    
+    for(int i=0;i<=n;i++){
+        if(nums[i] > 0){
+            for(int j=1;j<=nums[i];j++){
+                cout<<i << " ";
+            }
+        }
+    }
+    br;
+    
     
     return 0;
 }
